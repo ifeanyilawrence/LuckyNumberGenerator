690c35ae25b97d6dd26884cad0708e0f50299c82
Lawrence <ifeanyilawrence.eze@gmail.com>
2018-09-29 11:02:27 +0100

    luckyNumberGen v2

--- LuckyNumber/Form1.Designer.cs
+++ LuckyNumber/Form1.Designer.cs
@@ -39,32 +39,36 @@
             this.btnGenerateNumbers = new System.Windows.Forms.Button();
             this.btnClearNumbers = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
             this.SuspendLayout();
             // 
             // lblNumber
             // 
             this.lblNumber.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNumber.Location = new System.Drawing.Point(42, 33);
+            this.lblNumber.Location = new System.Drawing.Point(56, 41);
+            this.lblNumber.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNumber.Name = "lblNumber";
-            this.lblNumber.Size = new System.Drawing.Size(298, 33);
+            this.lblNumber.Size = new System.Drawing.Size(397, 41);
             this.lblNumber.TabIndex = 0;
             this.lblNumber.Text = "How many numbers to guess?";
             // 
             // tbxNumber
             // 
             this.tbxNumber.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbxNumber.Location = new System.Drawing.Point(346, 33);
+            this.tbxNumber.Location = new System.Drawing.Point(461, 41);
+            this.tbxNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbxNumber.Multiline = true;
             this.tbxNumber.Name = "tbxNumber";
-            this.tbxNumber.Size = new System.Drawing.Size(121, 35);
+            this.tbxNumber.Size = new System.Drawing.Size(160, 42);
             this.tbxNumber.TabIndex = 1;
             // 
             // lblLuckyNumbers
             // 
             this.lblLuckyNumbers.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLuckyNumbers.Location = new System.Drawing.Point(42, 98);
+            this.lblLuckyNumbers.Location = new System.Drawing.Point(56, 121);
+            this.lblLuckyNumbers.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLuckyNumbers.Name = "lblLuckyNumbers";
-            this.lblLuckyNumbers.Size = new System.Drawing.Size(298, 33);
+            this.lblLuckyNumbers.Size = new System.Drawing.Size(397, 41);
             this.lblLuckyNumbers.TabIndex = 2;
             this.lblLuckyNumbers.Text = "Your lucky numbers:";
             // 
@@ -72,76 +76,84 @@
             // 
             this.lbxLuckyNumbers.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbxLuckyNumbers.FormattingEnabled = true;
-            this.lbxLuckyNumbers.ItemHeight = 22;
-            this.lbxLuckyNumbers.Location = new System.Drawing.Point(47, 150);
+            this.lbxLuckyNumbers.ItemHeight = 26;
+            this.lbxLuckyNumbers.Location = new System.Drawing.Point(63, 185);
+            this.lbxLuckyNumbers.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lbxLuckyNumbers.Name = "lbxLuckyNumbers";
-            this.lbxLuckyNumbers.Size = new System.Drawing.Size(178, 158);
+            this.lbxLuckyNumbers.Size = new System.Drawing.Size(236, 186);
             this.lbxLuckyNumbers.TabIndex = 3;
             // 
             // lblLargestNumber
             // 
             this.lblLargestNumber.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLargestNumber.Location = new System.Drawing.Point(341, 150);
+            this.lblLargestNumber.Location = new System.Drawing.Point(455, 185);
+            this.lblLargestNumber.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLargestNumber.Name = "lblLargestNumber";
-            this.lblLargestNumber.Size = new System.Drawing.Size(191, 35);
+            this.lblLargestNumber.Size = new System.Drawing.Size(255, 43);
             this.lblLargestNumber.TabIndex = 4;
             this.lblLargestNumber.Text = "Largest number is:";
             // 
             // tbxLargestNumber
             // 
             this.tbxLargestNumber.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbxLargestNumber.Location = new System.Drawing.Point(538, 150);
+            this.tbxLargestNumber.Location = new System.Drawing.Point(717, 185);
+            this.tbxLargestNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbxLargestNumber.Multiline = true;
             this.tbxLargestNumber.Name = "tbxLargestNumber";
-            this.tbxLargestNumber.Size = new System.Drawing.Size(121, 35);
+            this.tbxLargestNumber.Size = new System.Drawing.Size(160, 42);
             this.tbxLargestNumber.TabIndex = 5;
             // 
             // tbxSmallestNumber
             // 
             this.tbxSmallestNumber.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbxSmallestNumber.Location = new System.Drawing.Point(538, 224);
+            this.tbxSmallestNumber.Location = new System.Drawing.Point(717, 276);
+            this.tbxSmallestNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbxSmallestNumber.Multiline = true;
             this.tbxSmallestNumber.Name = "tbxSmallestNumber";
-            this.tbxSmallestNumber.Size = new System.Drawing.Size(121, 35);
+            this.tbxSmallestNumber.Size = new System.Drawing.Size(160, 42);
             this.tbxSmallestNumber.TabIndex = 7;
             // 
             // lblSmallestNumber
             // 
             this.lblSmallestNumber.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSmallestNumber.Location = new System.Drawing.Point(341, 224);
+            this.lblSmallestNumber.Location = new System.Drawing.Point(455, 276);
+            this.lblSmallestNumber.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSmallestNumber.Name = "lblSmallestNumber";
-            this.lblSmallestNumber.Size = new System.Drawing.Size(191, 33);
+            this.lblSmallestNumber.Size = new System.Drawing.Size(255, 41);
             this.lblSmallestNumber.TabIndex = 6;
             this.lblSmallestNumber.Text = "Smallest number is:";
             // 
             // btnGenerateNumbers
             // 
             this.btnGenerateNumbers.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGenerateNumbers.Location = new System.Drawing.Point(47, 376);
+            this.btnGenerateNumbers.Location = new System.Drawing.Point(63, 463);
+            this.btnGenerateNumbers.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnGenerateNumbers.Name = "btnGenerateNumbers";
-            this.btnGenerateNumbers.Size = new System.Drawing.Size(267, 39);
+            this.btnGenerateNumbers.Size = new System.Drawing.Size(373, 48);
             this.btnGenerateNumbers.TabIndex = 8;
-            this.btnGenerateNumbers.Text = "Generate And Save Numbers";
+            this.btnGenerateNumbers.Text = "Generate And Save Lucky Number(s)";
             this.btnGenerateNumbers.UseVisualStyleBackColor = true;
             this.btnGenerateNumbers.Click += new System.EventHandler(this.btnGenerateNumbers_Click);
             // 
             // btnClearNumbers
             // 
             this.btnClearNumbers.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClearNumbers.Location = new System.Drawing.Point(346, 376);
+            this.btnClearNumbers.Location = new System.Drawing.Point(461, 463);
+            this.btnClearNumbers.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnClearNumbers.Name = "btnClearNumbers";
-            this.btnClearNumbers.Size = new System.Drawing.Size(174, 39);
+            this.btnClearNumbers.Size = new System.Drawing.Size(249, 48);
             this.btnClearNumbers.TabIndex = 9;
-            this.btnClearNumbers.Text = "Clear Numbers";
+            this.btnClearNumbers.Text = "Clear Form Information";
             this.btnClearNumbers.UseVisualStyleBackColor = true;
             this.btnClearNumbers.Click += new System.EventHandler(this.btnClearNumbers_Click);
             // 
             // btnExit
             // 
             this.btnExit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.Location = new System.Drawing.Point(569, 399);
+            this.btnExit.Location = new System.Drawing.Point(736, 463);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(115, 39);
+            this.btnExit.Size = new System.Drawing.Size(141, 48);
             this.btnExit.TabIndex = 10;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -149,9 +161,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(696, 450);
+            this.ClientSize = new System.Drawing.Size(928, 554);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnClearNumbers);
             this.Controls.Add(this.btnGenerateNumbers);
@@ -163,7 +175,9 @@
             this.Controls.Add(this.lblLuckyNumbers);
             this.Controls.Add(this.tbxNumber);
             this.Controls.Add(this.lblNumber);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lucky Number Generator";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -183,6 +197,7 @@
         private System.Windows.Forms.Button btnGenerateNumbers;
         private System.Windows.Forms.Button btnClearNumbers;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.SaveFileDialog saveFile;
     }
 }
 
--- LuckyNumber/Form1.cs
+++ LuckyNumber/Form1.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LuckyNumber
@@ -21,84 +13,153 @@ namespace LuckyNumber
 
         private void btnGenerateNumbers_Click(object sender, EventArgs e)
         {
-            int numberEntered = 0;
+            //Declare a variable to hold the user's input
+            int numberEntered;
 
+            //Declare a variable to hold each generated random number
+            int generatedRandomNumber;
+
+            //Declares a StreamWriter and a StreamReader variable
+            StreamWriter fileToWrite;
+            StreamReader fileToRead;
+
+            //Declears and initializes variables to hold the largest and smallest number
+            int largestNumber = 0;
+            int smallestNumber = 45; //45 is chosen because its the largest possible number
+
+            //Declare a variable to hold the read number
+            string readNumber;
+
+            //Validates input from the number textbox
             if (int.TryParse(tbxNumber.Text, out numberEntered))
             {
-                if (numberEntered <= 0)
+                //Checks if number entered is greater than zero
+                if (numberEntered > 0)
                 {
-                    MessageBox.Show("Number must be greater than zero.", "Warning!");
-                    tbxNumber.Focus();
-                    return;
-                }
+                    //Initializes the name of the file to be saved
+                    saveFile.FileName = "Numbers.txt";
+                    //Sets the default file extension
+                    saveFile.DefaultExt = "*.txt";
+                    //Sets the savefile filter
+                    saveFile.Filter = "Text Files|*.txt";
+                    //Sets the file's initial directory
+                    saveFile.InitialDirectory = @"C:\Documents";
+                    saveFile.RestoreDirectory = true;
 
-                string randomNumbersToSave = null;
-                List<int> randomNumbers = new List<int>();
-
-                for (int i = 0; i < numberEntered; i++)
-                {
-                    int randomNumber = GenerateRandomNumbers();
-                    randomNumbersToSave += i == (numberEntered-1) ?  randomNumber.ToString() : (randomNumber.ToString() + " ");
-
-                    randomNumbers.Add(randomNumber);
-
-                    Thread.Sleep(50);
-                }
-
-                try
-                {
-                    SaveFileDialog fileDialog = new SaveFileDialog();
-                    fileDialog.FileName = "Numbers.txt";
-                    fileDialog.DefaultExt = "*.txt";
-                    fileDialog.Filter = "Text Files|*.txt";
-                    fileDialog.ShowDialog();
-
-                    if (fileDialog.FileName == null || !fileDialog.FileName.ToLower().Contains("numbers.txt"))
+                    //Displays a Save as dialog box and checks if the user cancels the operation or not
+                    if (saveFile.ShowDialog() == DialogResult.OK)
                     {
-                        MessageBox.Show("Invalid filename.", "Warning!");
-                        tbxNumber.Focus();
-                        return;
+                        //Validates the file name
+                        if (saveFile.FileName.ToLower().Contains(@"\numbers.txt"))
+                        {
+                            try
+                            {
+                                //Create the file and get a StreamWriter object
+                                fileToWrite = File.CreateText(saveFile.FileName);
+
+                                //Create a Random object.
+                                Random rand = new Random();
+
+                                //Generates the lucky numbers and writes to the file
+                                for (int i = 0; i < numberEntered; i++)
+                                {
+                                    //Generates a random number between 1 and 45. 1 and 45 inclusive
+                                    generatedRandomNumber = rand.Next(45) + 1;
+
+                                    //Writes the generated lucky number to file
+                                    fileToWrite.WriteLine(generatedRandomNumber);
+
+                                    //Thread.Sleep(50);
+                                }
+
+                                //Close the file
+                                fileToWrite.Close();
+
+                                //Open the file and get a StreamReader object.
+                                fileToRead = File.OpenText(saveFile.FileName);
+
+                                //Clears the listbox items
+                                lbxLuckyNumbers.Items.Clear();
+
+                                //Reads the file till the end
+                                while (!fileToRead.EndOfStream)
+                                {
+                                    //Reads a character and assigns it to the readNumber variable
+                                    readNumber = fileToRead.ReadLine();
+
+                                    //Add the read number to listbox items
+                                    lbxLuckyNumbers.Items.Add(readNumber);
+
+                                    //checks if read number is greater than the set largest number, and assigns it as largest if true
+                                    if (int.Parse(readNumber) > largestNumber)
+                                        largestNumber = int.Parse(readNumber);
+
+                                    //checks if read number is smaller than the set smaller number, and assigns it as smallest if true
+                                    if (int.Parse(readNumber) < smallestNumber)
+                                        smallestNumber = int.Parse(readNumber);
+                                }
+
+                                //Closes the file
+                                fileToRead.Close();
+
+                                //Assigns the largest number
+                                tbxLargestNumber.Text = largestNumber.ToString();
+
+                                //Assigns the smallest number
+                                tbxSmallestNumber.Text = smallestNumber.ToString();
+
+                                tbxNumber.Focus();
+                            }
+                            catch (Exception ex)
+                            {
+                                //Display an error message
+                                MessageBox.Show(ex.Message);
+                                //Returns focus to the number of lucky numbers textbox
+                                tbxNumber.Focus();
+                            }
+                        }
+                        else
+                        {
+                            //Display an error message to the user
+                            MessageBox.Show("Invalid filename.");
+                            //Returns focus to the number of lucky numbers textbox
+                            tbxNumber.Focus();
+                        }
                     }
-
-                    File.WriteAllText(fileDialog.FileName, randomNumbersToSave);
+                    else
+                    {
+                        //Display a cancel message to the user
+                        MessageBox.Show("Operation canceled.");
+                        //Returns focus to the number of lucky numbers textbox
+                        tbxNumber.Focus();
+                    }
                 }
-                catch (Exception ex)
+                else
                 {
-                    MessageBox.Show(ex.Message, "File Operation Error!");
+                    //Display an error message to the user
+                    MessageBox.Show("Number must be greater than zero.");
+
+                    //Returns focus to the number of lucky numbers textbox
                     tbxNumber.Focus();
-                    return;
                 }
-                
-                lbxLuckyNumbers.DataSource = randomNumbers;
-
-                tbxLargestNumber.Text = randomNumbers.Max().ToString();
-                tbxSmallestNumber.Text = randomNumbers.Min().ToString();
-
-                tbxNumber.Focus();
             }
             else
             {
-                MessageBox.Show("Invalid number entered, kindly check and try again.", "Warning!");
+                //Display an error message to the user
+                MessageBox.Show("Invalid number entered, kindly check and try again.");
+
+                //Returns focus to the number of lucky numbers textbox
                 tbxNumber.Focus();
-                return;
             }
         }
-
-        private int GenerateRandomNumbers()
-        {
-            Random random = new Random();
-            return random.Next(1, 46);
-        }
-
         private void btnClearNumbers_Click(object sender, EventArgs e)
         {
             tbxNumber.Text = "";
             tbxSmallestNumber.Text = "";
             tbxLargestNumber.Text = "";
-            lbxLuckyNumbers.DataSource = null;
+            lbxLuckyNumbers.Items.Clear();
             tbxNumber.Focus();
         }
-
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
